2ff7213168ec5bac69b388f0957a9269b796ca3d
ochiengsulwe <kodongochieng@gmail.com>
2024-02-29 16:15:24 +0300

    downgrading werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
+# Install flask
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+# Install Werkzeug 2.0.
+package { 'werkzeug':
+    ensure   => '2.0.3',
+    provider => 'pip3',
 }
